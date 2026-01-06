54f1903f5e7e0d2f90e2a1cade41fb5df1dabecb
Keith Sialana <keith.sialana@hotmail.com>
2026-01-06 05:46:09 -0500

    small change, added loading text when list is actively generating

--- src/components/playlist.jsx
+++ src/components/playlist.jsx
@@ -29,16 +29,18 @@ export default function Playlist() {
           playlistMood +
           ". Respond with a random list of 49 song titles and their respective artists that fit this mood, separated by semicolons. Format the response as 'Song Title - Artist Name'. Do not include any additional text or explanations.",
       });
-      setPlaylist("Generating playlist...");
+      document.getElementById('loadingText').removeAttribute("hidden");
+      setPlaylist([]);
       const text = await result.getText();
-
+      
       // Convert semicolon-separated string into an array
       let playlistList = text.split(";").map((song) => song.trim());
-
-      setPlaylist(playlistList); // Update the state with the generated playlist
+      setPlaylist(playlistList);
+      document.getElementById('loadingText').setAttribute("hidden","true");
+      console.log('song list updated')
     } catch (error) {
       console.error("Error generating playlist:", error);
-      setPlaylist("Error generating playlist. Please try again.");
+
     }
   }
 
@@ -49,17 +51,19 @@ export default function Playlist() {
           ? `Your '` + playlistMood + `' playlist:`
           : "Set your playlist!"}
       </h1>
-      <div>
-        {playlistList.length > 0 ? (
+      <h3 id="loadingText" hidden>Generating your playlist... Hang on!</h3>
+      <div id="songsText">
+        {playlistList.length < 1 ? "Create your playlist!":
+        (
           <ol>
             {playlistList.map((song, index) => (
               <li key={index}>{song}</li>
             ))}
           </ol>
-        ) : (
-          ""
         )}
       </div>
+      <h2>Notice</h2>
+      <p>There was a plan to integrate creating a playlist using Spotify, but as of 2024, Spotify has disabled any use of their API unless you're a business that makes money and has a lot of streams, so I'm sorry to the users. You're going to just have to copy and paste this list somewhere else and manually make the playlist on Spotify :(</p>
     </div>
   );
 }
